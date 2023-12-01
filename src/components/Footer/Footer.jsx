f3bc6697d088cd181f86baf871192e8de8e8f1ca
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-01 16:37:32 -0300

    Creating Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+
+import styled from '@emotion/styled';
+
+export const Footer = styled.footer`
+    background: ${props => props.theme.colors.primary.a};
+    padding: ${props => props.theme.spacing.xl};
+    color: ${props => props.theme.colors.white};
+`;
