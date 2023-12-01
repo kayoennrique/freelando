eef61984f84b755a3fbc0b5ec1042538596df098
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-01 16:22:08 -0300

    Typography Styles

--- src/components/Typography/Typography.jsx
+++ src/components/Typography/Typography.jsx
@@ -2,19 +2,55 @@ import styled from "@emotion/styled"
 
 const components = {
     h1: 'h1',
-    body: 'p'
+    h2: 'h2',
+    h3: 'h3',
+    body: 'p',
+    bodyBold: 'strong',
+    body2: 'p',
+    body2Bold: 'strong',
+    subtitle: 'p'
 }
 
 const styles = {
     h1: `
-    font-weight: 600;
-    font-size: 40px;
-    line-height: 49px;
+        font-weight: 600;
+        font-size: 40px;
+        line-height: 49px;
+    `,
+    h2: `
+        font-weight: 600;
+        font-size: 32px;
+        line-height: 39px;
+    `,
+    h3: `
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 29px;
     `,
     body: `
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 24px;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 24px;
+    `,
+    bodyBold: `
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 24px;
+    `,
+    body2: `
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 20px;
+    `,
+    body2Bold: `
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 20px;
+    `,
+    subtitle: `
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 17px;
     `
 }
 
