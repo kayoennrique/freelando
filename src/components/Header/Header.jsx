2a75009ea3ebf0623e81227b7c6f520cba4851c5
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-01 16:04:00 -0300

    Creating Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const Header = styled.header`
+    background: ${props => props.theme.colors.primary.a};
+    padding: ${props => props.theme.spacing.m};
+`
