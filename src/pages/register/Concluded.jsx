79e69e385639de673116f6d66854e73fc2f034de
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-02 14:58:49 -0300

    Bug fix

--- src/pages/register/Concluded.jsx
+++ src/pages/register/Concluded.jsx
@@ -1,7 +1,7 @@
 import { Col, Row } from "react-grid-system";
 import { Button } from "../../components/Button/Button";
 import { Typography } from "../../components/Typography/Typography";
-import { Link } from "../../components/Link/Link";
+import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 
 import conclusion from './assets/client-completed.png';
@@ -12,28 +12,29 @@ const StylizedImage = styled.img`
 `;
 
 const Concluded = () => {
-    return (<>
-        <div style={{ textAlign: 'center' }}>
-            <Typography variant="h1" component="h1">
-                Seu perfil está completo!
-            </Typography>
-            <Typography variant="h3" component="h3">
-                Agora é só começar a se conectar com os melhores freelancers do mercado!
-            </Typography>
-        </div>
-        <figure>
-            <StylizedImage src={conclusion} alt="" />
-        </figure>
-        <Row justify="center">
-            <Col lg={6} md={6} sm={6} style={{ textAlign: 'center' }}>
-                <Link to='/cadastro'>
-                    <Button variant="secondary">
-                        Voltar para a home
-                    </Button>
-                </Link>
-            </Col>
-        </Row>
-    </>
+    return (
+        <>
+            <div style={{ textAlign: 'center' }}>
+                <Typography variant="h1" component="h1">
+                    Seu perfil está completo!
+                </Typography>
+                <Typography variant="h3" component="h3">
+                    Agora é só começar a se conectar com os melhores freelancers do mercado!
+                </Typography>
+            </div>
+            <figure>
+                <StylizedImage src={conclusion} alt="" />
+            </figure>
+            <Row justify="center">
+                <Col lg={6} md={6} sm={6} style={{ textAlign: 'center' }}>
+                    <Link to='/cadastro'>
+                        <Button variant="secondary">
+                            Voltar para a home
+                        </Button>
+                    </Link>
+                </Col>
+            </Row>
+        </>
     );
 }
 
